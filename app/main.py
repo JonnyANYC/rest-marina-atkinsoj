59692cc577370d0df9b3ad698bff1aa97aa5a6c8
JonnyA <jon@angelajonhome.com>
2018-04-29 02:34:53 -0700

    Changed the dock/undock actions to live at the /boats/:id/docked sub-entity. Also fixed a couple bugs after completing the full test suite.

--- app/main.py
+++ app/main.py
@@ -19,7 +19,9 @@ class Boat(ndb.Model):
 
     def to_json_ready(self):
         boat_json_ready = dict(id=self.key.id(), name=self.name, type=self.type, length=self.length,
-                               at_sea=self.at_sea, self="/boats/" + str(self.key.id()))
+                               at_sea=self.at_sea, self="/boats/" + str(self.key.id()),
+                               docked_url="/boats/" + str(self.key.id()) + "/docked")
+
         return boat_json_ready
 
 
@@ -39,10 +41,9 @@ class BoatListHandler(webapp2.RequestHandler):
 
         request_data = json.loads(self.request.body)
 
-        # FIXME: Add error handling for missing fields. Need to test for property existence in request_data.
-
         # TODO: If possible, switch the auto-generated ID to a string ID that is generated here.
 
+        # TODO: Not thread-safe!! But probably not an issue for this assignment.
         existing = Boat.query().filter(Boat.name == request_data["name"]).fetch(1)
         if existing:
             send_error(self.response, 400,
@@ -101,29 +102,17 @@ class BoatHandler(webapp2.RequestHandler):
         if "length" in request_data and request_data["length"]:
             boat.length = request_data["length"]
 
-        if "at_sea" in request_data and request_data["at_sea"]:
-            if request_data["at_sea"] == "false":
-
-                # FIXME: This is all old and prolly untested and not needed here any more.
-                self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
-                self.response.write(response_message_json("FAILURE", "Use PATCH /slips/:id to dock a boat in a slip."))
+        if "at_sea" in request_data:
+                send_error(self.response, 404,
+                           response_message_json("FAILURE", "Use PATCH /boat/:id/dock to dock a boat."))
                 return
-
-            if request_data["at_sea"] == "true" and not boat.at_sea:
-
-                slip = Slip.find_slip_by_boat_id(boat_id)
-                self.response.write(response_message_json("SUCCESS sorta", "Found it! slip # " + slip.number + " has boat # " + slip.current_boat))
-                slip.current_boat = None
-                slip.arrival_date = None
-                slip.put()
-                boat.at_sea = True
 
         boat.put()
 
         send_success(self.response, json.dumps(boat.to_json_ready()))
 
 
-class BoatDockHandler(webapp2.RequestHandler):
+class BoatDockedHandler(webapp2.RequestHandler):
 
     def patch(self, boat_id):
 
@@ -134,18 +123,16 @@ class BoatDockHandler(webapp2.RequestHandler):
 
         if "slip_id" not in request_data:
             send_error(self.response, 400, response_message_json("FAILURE", "slip_id property missing. " 
-                       "Provide empty slip_id to set a boat at sea."))
+                       "Provide empty slip_id to set a boat at sea, or DELETE to the same entity."))
             return
 
-        # send_success(self.response, "slip id: " + request_data["slip_id"])
-        # send_success(self.response, "slip is null? " + str(request_data["slip_id"] is None))
         if request_data["slip_id"] is None:
             # The boat is leaving a slip. Validate first.
             if boat.at_sea:
                 send_error(self.response, 400, response_message_json("FAILURE", "The boat is already at sea."))
                 return
 
-            current_slip = Slip.find_slip_by_boat_id(boat_id)
+            current_slip = Slip.find_slip_by_boat_id(long(boat_id))
 
             if not current_slip:
                 send_error(self.response, 400, response_message_json("FAILURE", "The boat is not at sea and not docked."))
@@ -237,10 +224,6 @@ class SlipListHandler(webapp2.RequestHandler):
     def post(self):
         request_data = json.loads(self.request.body)
 
-        # FIXME: Add error handling for missing fields. Need to test for property existence in request_data.
-
-        # FIXME: Set the arrival_date to now(), but in the required string format
-
         # Don't set current_boat. "All newly created slips should be empty."
         slip = Slip(number=int(request_data["number"]))
         slip.put()
@@ -324,7 +307,7 @@ webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 app = webapp2.WSGIApplication([
     ('/boats', BoatListHandler),
     ('/boats/(\d+)', BoatHandler),
-    ('/boats/(\d+)/dock', BoatDockHandler),
+    ('/boats/(\d+)/docked', BoatDockedHandler),
     ('/slips', SlipListHandler),
     ('/slips/(\d+)', SlipHandler),
 ], debug=True)
